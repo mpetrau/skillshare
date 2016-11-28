ca1bdca26fea9a1d6b9e0e4de91138c7c63ec4f6
Mykolas Petrauskas <petrauskas.mykolas@gmail.com>
2016-11-28 16:25:30 +0100

    finalized User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
   has_many :offers
-  has_many :bookings, through: :offers
   has_many :bookings
-
-
+  has_many :booked_offers, through: :offers, source: :bookings
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
