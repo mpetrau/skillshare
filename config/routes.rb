92129066fdd03aa3bd11b0759b1983f77d0a6ff4
Mykolas Petrauskas <petrauskas.mykolas@gmail.com>
2016-11-29 19:57:48 +0100

    created profile router, controller, and basic view

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
   devise_for :users
+  resource :profile, only: [:show]
   resources :offers, only: [:index, :show, :new, :create] do
     resources :bookings, only: [:create]
     end
