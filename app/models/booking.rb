74791366da42ead8248d271043c49a4357b80ff4
Matilda Waters <matilda.waters1@gmail.com>
2016-11-29 11:04:18 +0000

    validations updated

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :offer
+  validates :week, presence: true
+  validates :user, presence: true
+  validates :offer, presence: true
 end
--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,9 +1,22 @@
 class Offer < ApplicationRecord
-  belongs_to :user
-  has_many :bookings
-
+  
   SKILLS_CATS = %w(Cooking Photography Paragliding Programming Jogging)
   SENIORITY = %w(new junior experienced advanced expert)
   LOCATIONS = %w(Barcelona Madrid Valencia Malaga)
+
+  belongs_to :user
+  has_many :bookings
+  before_destroy :check_for_bookings
+  validates :user, :price, :title, :description, :seniority, :category, :location, presence: true
+  validates :price, numericality: { only_integer: true }
+  validates_inclusion_of :seniority, in: SENIORITY
+  validates_inclusion_of :category, in: SKILLS_CATS
+  
+  private
+  def check_for_bookings
+    unless bookings.nil?
+      flash[:warning] = "Cannot delete offer when bookings are still pending, pleas cancel your bookings first"
+    end
+  end
 
 end
