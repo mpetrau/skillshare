c4c0fcfc0f841b7da0aa167ab1a14c4b3594b177
Mykolas Petrauskas <petrauskas.mykolas@gmail.com>
2016-11-29 12:28:49 +0100

    created new offers form

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -9,4 +9,26 @@ class OffersController < ApplicationController
     @offer = Offer.find(params[:id])
     @booking = Booking.new
   end
+
+  def new
+    @offer = Offer.new
+  end
+
+  def create
+    @offer = Offer.new(offer_params)
+    @offer.user = current_user
+    if @offer.save
+      redirect_to @offer
+      flash[:notice] = "Your offer was created"
+    else
+      flash[:warning] = "There is a mistake on your form"
+      render :new
+    end
+  end
+
+  private
+
+  def offer_params
+    params.require(:offer).permit([:price, :title, :description, :seniority, :category, :location])
+  end
 end
