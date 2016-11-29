25647f52c74d29c0e1218d8de8930f48f1019752
Matilda Waters <matilda.waters1@gmail.com>
2016-11-29 11:30:25 +0000

    validations redone

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-
+  before_destroy :check_for_bookings
   has_many :offers, dependent: :destroy
   has_many :bookings
   has_many :booked_offers, through: :offers, source: :bookings
-  validates :name, :picture, presence: true
+  # validates :name, :picture, presence: true
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
