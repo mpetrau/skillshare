ed5c77f61489d30bf0c72f65c8919e681d66a1b1
Jessica Smith <adamcarabetta@gmail.com>
2016-12-02 16:46:34 +0100

    Final Preps

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,8 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
-  has_one :review
   belongs_to :offer
-  has_one :review
   validates :date, presence: true
   validates :user, presence: true
   validates :offer, presence: true
