ae364035118b6213954677d67cd4b15c5ee58792
Matilda Waters <matilda.waters1@gmail.com>
2016-12-02 10:36:22 +0000

    bug fixed on pages controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,6 +3,6 @@ class ProfilesController < ApplicationController
     @profile = current_user
     @booked_offers = @profile.booked_offers
     @bookings = @profile.bookings
-    @all_bookings = (@booked_offers).zip(@bookings).flatten.sort { |a,b| b.date <=> a.date }
+    @all_bookings = (@booked_offers + @bookings).sort { |a,b| b.date <=> a.date }
   end
 end
