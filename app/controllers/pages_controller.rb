16187bdaaf42baa71e5271260e5fc95850499411
Matilda Waters <matilda.waters1@gmail.com>
2016-12-02 10:27:56 +0000

    pages controller bug fixed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,7 +6,7 @@ class PagesController < ApplicationController
     if user_signed_in?
       booked_offers = current_user.booked_offers
       bookings = current_user.bookings
-      @all_bookings = (booked_offers).zip(bookings).flatten.sort { |a,b| a.created_at <=> b.created_at }.first(4)
+      @all_bookings = (booked_offers + bookings).sort { |a,b| a.created_at <=> b.created_at }.first(4)
     else
       @all_bookings = nil
     end
