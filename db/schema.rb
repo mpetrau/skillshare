678105687385786a995d0a2a681fa0d7fb14fc80
Jessica Smith <adamcarabetta@gmail.com>
2016-12-02 11:40:03 +0100

    Adding Reviews to Profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161201214732) do
+ActiveRecord::Schema.define(version: 20161202020741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,16 @@ ActiveRecord::Schema.define(version: 20161201214732) do
     t.date     "date"
     t.index ["offer_id"], name: "index_bookings_on_offer_id", using: :btree
     t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
+  end
+
+  create_table "offer_reviews", force: :cascade do |t|
+    t.string   "title"
+    t.string   "description"
+    t.integer  "rating"
+    t.integer  "offer_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["offer_id"], name: "index_offer_reviews_on_offer_id", using: :btree
   end
 
   create_table "offers", force: :cascade do |t|
@@ -53,6 +63,15 @@ ActiveRecord::Schema.define(version: 20161201214732) do
     t.index ["booking_id"], name: "index_reviews_on_booking_id", using: :btree
   end
 
+  create_table "user_reviews", force: :cascade do |t|
+    t.string   "title"
+    t.string   "description"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["user_id"], name: "index_user_reviews_on_user_id", using: :btree
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "photo"
@@ -81,6 +100,8 @@ ActiveRecord::Schema.define(version: 20161201214732) do
 
   add_foreign_key "bookings", "offers"
   add_foreign_key "bookings", "users"
+  add_foreign_key "offer_reviews", "offers"
   add_foreign_key "offers", "users"
   add_foreign_key "reviews", "bookings"
+  add_foreign_key "user_reviews", "users"
 end
